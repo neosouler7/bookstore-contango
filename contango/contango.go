e0c96d1335128c6ba81076ed43c3624f7a69753c
JH <neosouler@gmail.com>
2024-12-28 19:24:46 +0900

    suit for short&long

--- contango/contango.go
+++ contango/contango.go
@@ -8,6 +8,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/gorilla/websocket"
 	"github.com/neosouler7/bookstore-contango/binance"
 	"github.com/neosouler7/bookstore-contango/bybit"
 	"github.com/neosouler7/bookstore-contango/commons"
@@ -146,8 +147,26 @@ func pong() {
 		}
 		for _, exchange := range []string{"byb", "byf"} {
 			websocketmanager.SendMsg(exchange, "{\"op\": \"ping\"")
+
+			// 추후 문제 생기면 아래 코드 참고
+			// pingMessage := map[string]interface{}{
+			// 	"req_id": "100001", // 사용자 정의 ID
+			// 	"op":     "ping",
+			// }
+			// messageBytes, err := json.Marshal(pingMessage)
+			// if err != nil {
+			// 	log.Printf("Failed to encode ping message: %v", err)
+			// 	continue
+			// }
+
+			// // Ping 메시지 전송
+			// err = conn.WriteMessage(websocket.TextMessage, messageBytes)
+			// if err != nil {
+			// 	log.Printf("Failed to send Ping message: %v", err)
+			// 	return
+			// }
 		}
-		log.Println("ping")
+		// log.Println("ping")
 		time.Sleep(time.Second * 5)
 	}
 }
@@ -203,121 +222,143 @@ func subscribe(pairMap map[string][]string) {
 	close(subscribeDone) // 모든 websocket 구독 요청 완료
 }
 
-func receiveBin(exchange string) {
-	for {
-		_, msgBytes, err := websocketmanager.Conn(exchange).ReadMessage()
-		tgmanager.HandleErr(exchange, err)
+// func receiveBin(exchange string) {
+// 	for {
+// 		_, msgBytes, err := websocketmanager.Conn(exchange).ReadMessage()
+// 		tgmanager.HandleErr(exchange, err)
 
-		if strings.Contains(string(msgBytes), "result") {
-			fmt.Printf(websocketmanager.FilteredMsg, exchange, string(msgBytes))
-		} else {
-			var rJson interface{}
-			commons.Bytes2Json(msgBytes, &rJson)
+// 		if strings.Contains(string(msgBytes), "result") {
+// 			fmt.Printf(websocketmanager.FilteredMsg, exchange, string(msgBytes))
+// 		} else {
+// 			var rJson interface{}
+// 			commons.Bytes2Json(msgBytes, &rJson)
 
-			data := rJson.(map[string]interface{})["data"]
-			askR, bidR := data.(map[string]interface{})["asks"], data.(map[string]interface{})["bids"]
-			askPrice, bidPrice := askR.([]interface{})[1].([]interface{})[0], bidR.([]interface{})[1].([]interface{})[0] // 2번째 호가의 가격
-			ts, pair := strconv.FormatInt(time.Now().UnixNano()/1000000, 10), strings.Split(rJson.(map[string]interface{})["stream"].(string), "@")[0]
-			market, symbol := "usdt", pair[:len(pair)-4]
+// 			data := rJson.(map[string]interface{})["data"]
+// 			askR, bidR := data.(map[string]interface{})["asks"], data.(map[string]interface{})["bids"]
+// 			askPrice, bidPrice := askR.([]interface{})[1].([]interface{})[0], bidR.([]interface{})[1].([]interface{})[0] // 2번째 호가의 가격
+// 			ts, pair := strconv.FormatInt(time.Now().UnixNano()/1000000, 10), strings.Split(rJson.(map[string]interface{})["stream"].(string), "@")[0]
+// 			market, symbol := "usdt", pair[:len(pair)-4]
 
-			//fmt.Printf("%s|%s|%s|%s|%s|%s\n", exchange, market, symbol, askPrice, bidPrice, ts)
+// 			//fmt.Printf("%s|%s|%s|%s|%s|%s\n", exchange, market, symbol, askPrice, bidPrice, ts)
 
-			key := fmt.Sprintf("%s:%s:%s", exchange, market, symbol)
-			value := fmt.Sprintf("%s:%s:%s", askPrice, bidPrice, ts)
-			syncMap.Store(key, value)
-		}
-	}
-}
+// 			key := fmt.Sprintf("%s:%s:%s", exchange, market, symbol)
+// 			value := fmt.Sprintf("%s:%s:%s", askPrice, bidPrice, ts)
+// 			syncMap.Store(key, value)
+// 		}
+// 	}
+// }
 
 func receiveBif(exchange string) {
 	for {
-		_, msgBytes, err := websocketmanager.Conn(exchange).ReadMessage()
+		msgType, msgBytes, err := websocketmanager.Conn(exchange).ReadMessage()
 		tgmanager.HandleErr(exchange, err)
 
-		if strings.Contains(string(msgBytes), "result") {
-			fmt.Printf(websocketmanager.FilteredMsg, exchange, string(msgBytes))
-		} else {
-			var rJson interface{}
-			commons.Bytes2Json(msgBytes, &rJson)
+		switch msgType {
+		case websocket.PingMessage: // 실질적으로는 거치지 않음...
+			err = websocketmanager.Conn(exchange).WriteMessage(websocket.PongMessage, msgBytes)
+			if err != nil {
+				tgmanager.HandleErr(exchange, fmt.Errorf("failed to send Pong: %v", err))
+			}
+			tgmanager.SendMsg(fmt.Sprintf("%s received PING", exchange))
+			continue
 
-			data := rJson.(map[string]interface{})["data"]
-			askR, bidR := data.(map[string]interface{})["a"], data.(map[string]interface{})["b"]
-			askPrice, bidPrice := askR.([]interface{})[1].([]interface{})[0], bidR.([]interface{})[1].([]interface{})[0] // 2번째 호가의 가격
-			ts, pair := strconv.FormatInt(int64(data.(map[string]interface{})["T"].(float64)), 10), data.(map[string]interface{})["s"]
-			market, symbol := "usdt", strings.ToLower(pair.(string)[:len(pair.(string))-4])
+		case websocket.TextMessage, websocket.BinaryMessage:
+			if strings.Contains(string(msgBytes), "result") {
+				fmt.Printf(websocketmanager.FilteredMsg, exchange, string(msgBytes))
+			} else {
+				var rJson interface{}
+				commons.Bytes2Json(msgBytes, &rJson)
 
-			//fmt.Printf("%s|%s|%s|%s|%s|%s\n", exchange, market, symbol, askPrice, bidPrice, ts)
+				data := rJson.(map[string]interface{})["data"]
+				askR, bidR := data.(map[string]interface{})["a"], data.(map[string]interface{})["b"]
+				askPrice, bidPrice := askR.([]interface{})[1].([]interface{})[0], bidR.([]interface{})[1].([]interface{})[0] // 2번째 호가의 가격
+				ts, pair := strconv.FormatInt(int64(data.(map[string]interface{})["T"].(float64)), 10), data.(map[string]interface{})["s"]
+				market, symbol := "usdt", strings.ToLower(pair.(string)[:len(pair.(string))-4])
 
-			key := fmt.Sprintf("%s:%s:%s", exchange, market, symbol)
-			value := fmt.Sprintf("%s:%s:%s", askPrice, bidPrice, ts)
-			syncMap.Store(key, value)
-		}
-	}
-}
-
-func receiveByb(exchange string) {
-	for {
-		_, msgBytes, err := websocketmanager.Conn(exchange).ReadMessage()
-		tgmanager.HandleErr(exchange, err)
-
-		var rJson map[string]interface{}
-		commons.Bytes2Json(msgBytes, &rJson)
-
-		if _, ok := rJson["topic"].(string); ok {
-			if data, ok := rJson["data"].(map[string]interface{}); ok {
-				pair, ts := data["s"].(string), rJson["ts"].(float64)
-				market, symbol := "usdt", strings.ToLower(pair[:len(pair)-len("USDT")])
-
-				// Orderbook Update
-				bids := parseOrders(data["b"].([]interface{}))
-				asks := parseOrders(data["a"].([]interface{}))
-				// isSnapshot := rJson["type"].(string) == "snapshot"
-				updateOrderbook(&bybOrderbook, pair, bids, asks)
-
-				// 현재 ask/bid 가격 출력
-				bidPrice, askPrice := getBestPrices(&bybOrderbook, pair)
-				bidPriceStr, askPriceStr := strconv.FormatFloat(bidPrice, 'f', -1, 64), strconv.FormatFloat(askPrice, 'f', -1, 64)
-				tsStr := strconv.FormatFloat(ts, 'f', -1, 64)
-
-				//fmt.Printf("%s|%s|%s|%s|%s|%s\n", exchange, market, symbol, askPriceStr, bidPriceStr, tsStr)
+				// fmt.Printf("%s|%s|%s|%s|%s|%s\n", exchange, market, symbol, askPrice, bidPrice, ts)
 
 				key := fmt.Sprintf("%s:%s:%s", exchange, market, symbol)
-				value := fmt.Sprintf("%s:%s:%s", askPriceStr, bidPriceStr, tsStr)
+				value := fmt.Sprintf("%s:%s:%s", askPrice, bidPrice, ts)
 				syncMap.Store(key, value)
 			}
 		}
 	}
 }
 
+// func receiveByb(exchange string) {
+// 	for {
+// 		_, msgBytes, err := websocketmanager.Conn(exchange).ReadMessage()
+// 		tgmanager.HandleErr(exchange, err)
+
+// 		var rJson map[string]interface{}
+// 		commons.Bytes2Json(msgBytes, &rJson)
+
+// 		if _, ok := rJson["topic"].(string); ok {
+// 			if data, ok := rJson["data"].(map[string]interface{}); ok {
+// 				pair, ts := data["s"].(string), rJson["ts"].(float64)
+// 				market, symbol := "usdt", strings.ToLower(pair[:len(pair)-len("USDT")])
+
+// 				// Orderbook Update
+// 				bids := parseOrders(data["b"].([]interface{}))
+// 				asks := parseOrders(data["a"].([]interface{}))
+// 				// isSnapshot := rJson["type"].(string) == "snapshot"
+// 				updateOrderbook(&bybOrderbook, pair, bids, asks)
+
+// 				// 현재 ask/bid 가격 출력
+// 				bidPrice, askPrice := getBestPrices(&bybOrderbook, pair)
+// 				bidPriceStr, askPriceStr := strconv.FormatFloat(bidPrice, 'f', -1, 64), strconv.FormatFloat(askPrice, 'f', -1, 64)
+// 				tsStr := strconv.FormatFloat(ts, 'f', -1, 64)
+
+// 				//fmt.Printf("%s|%s|%s|%s|%s|%s\n", exchange, market, symbol, askPriceStr, bidPriceStr, tsStr)
+
+// 				key := fmt.Sprintf("%s:%s:%s", exchange, market, symbol)
+// 				value := fmt.Sprintf("%s:%s:%s", askPriceStr, bidPriceStr, tsStr)
+// 				syncMap.Store(key, value)
+// 			}
+// 		}
+// 	}
+// }
+
 func receiveByf(exchange string) {
 	for {
-		_, msgBytes, err := websocketmanager.Conn(exchange).ReadMessage()
+		msgType, msgBytes, err := websocketmanager.Conn(exchange).ReadMessage()
 		tgmanager.HandleErr(exchange, err)
 
-		var rJson map[string]interface{}
-		commons.Bytes2Json(msgBytes, &rJson)
+		switch msgType {
+		case websocket.PingMessage: // 실질적으로는 거치지 않음...
+			err = websocketmanager.Conn(exchange).WriteMessage(websocket.PongMessage, msgBytes)
+			if err != nil {
+				tgmanager.HandleErr(exchange, fmt.Errorf("failed to send Pong: %v", err))
+			}
+			tgmanager.SendMsg(fmt.Sprintf("%s received PING", exchange))
+			continue
 
-		if _, ok := rJson["topic"].(string); ok {
-			if data, ok := rJson["data"].(map[string]interface{}); ok {
-				pair, ts := data["s"].(string), rJson["ts"].(float64)
-				market, symbol := "usdt", strings.ToLower(pair[:len(pair)-len("USDT")])
+		case websocket.TextMessage, websocket.BinaryMessage:
+			var rJson map[string]interface{}
+			commons.Bytes2Json(msgBytes, &rJson)
 
-				// Orderbook Update
-				bids := parseOrders(data["b"].([]interface{}))
-				asks := parseOrders(data["a"].([]interface{}))
-				// isSnapshot := rJson["type"].(string) == "snapshot"
-				updateOrderbook(&byfOrderbook, pair, bids, asks)
+			if _, ok := rJson["topic"].(string); ok {
+				if data, ok := rJson["data"].(map[string]interface{}); ok {
+					pair, ts := data["s"].(string), rJson["ts"].(float64)
+					market, symbol := "usdt", strings.ToLower(pair[:len(pair)-len("USDT")])
 
-				// 현재 ask/bid 가격 출력
-				bidPrice, askPrice := getBestPrices(&byfOrderbook, pair)
-				bidPriceStr, askPriceStr := strconv.FormatFloat(bidPrice, 'f', -1, 64), strconv.FormatFloat(askPrice, 'f', -1, 64)
-				tsStr := strconv.FormatFloat(ts, 'f', -1, 64)
+					// Orderbook Update
+					bids := parseOrders(data["b"].([]interface{}))
+					asks := parseOrders(data["a"].([]interface{}))
+					// isSnapshot := rJson["type"].(string) == "snapshot"
+					updateOrderbook(&byfOrderbook, pair, bids, asks)
 
-				//fmt.Printf("%s|%s|%s|%s|%s|%s\n", exchange, market, symbol, askPriceStr, bidPriceStr, tsStr)
+					// 현재 ask/bid 가격 출력
+					bidPrice, askPrice := getBestPrices(&byfOrderbook, pair)
+					bidPriceStr, askPriceStr := strconv.FormatFloat(bidPrice, 'f', -1, 64), strconv.FormatFloat(askPrice, 'f', -1, 64)
+					tsStr := strconv.FormatFloat(ts, 'f', -1, 64)
 
-				key := fmt.Sprintf("%s:%s:%s", exchange, market, symbol)
-				value := fmt.Sprintf("%s:%s:%s", askPriceStr, bidPriceStr, tsStr)
-				syncMap.Store(key, value)
+					// fmt.Printf("%s|%s|%s|%s|%s|%s\n", exchange, market, symbol, askPriceStr, bidPriceStr, tsStr)
+
+					key := fmt.Sprintf("%s:%s:%s", exchange, market, symbol)
+					value := fmt.Sprintf("%s:%s:%s", askPriceStr, bidPriceStr, tsStr)
+					syncMap.Store(key, value)
+				}
 			}
 		}
 	}
@@ -351,85 +392,90 @@ func calShortLongPremium(pairMap map[string][]string) {
 					}
 
 					// Premium 계산(ask:bid:ts)
-					shortPrice, longPrice, _ := strings.Split(shortInfo.(string), ":")[1], strings.Split(longInfo.(string), ":")[0], strings.Split(longInfo.(string), ":")[2]
-					shotPriceFloat, _ := strconv.ParseFloat(shortPrice, 64)
+					shortPrice, shortTs := strings.Split(shortInfo.(string), ":")[1], strings.Split(shortInfo.(string), ":")[2]
+					longPrice, longTs := strings.Split(longInfo.(string), ":")[0], strings.Split(longInfo.(string), ":")[2]
+					shortPriceFloat, _ := strconv.ParseFloat(shortPrice, 64)
 					longPriceFloat, _ := strconv.ParseFloat(longPrice, 64)
-					premium := commons.RoundToDecimal((longPriceFloat-shotPriceFloat)/shotPriceFloat, 5) * 100
+					// premium := commons.RoundToDecimal((longPriceFloat-shortPriceFloat)/shortPriceFloat, 5) * 100 // long/short - 1 이 아니라(short이 클 때 비싸게 파는거니까 음수가 됨)
+					premium := commons.RoundToDecimal((shortPriceFloat-longPriceFloat)/longPriceFloat, 5) * 100 // short/long - 1 로 해야지 short이 커서 프리미엄이 양수가 됨
 
 					key := fmt.Sprintf("sl:%s:%s:%s:%s", shortE, longE, market, symbol)
-					value := fmt.Sprintf("%f", premium)
-					msg := fmt.Sprintf("%s -> %.6s (short: %.8s, long: %.8s)\n", key, value, shortPrice, longPrice)
+					value := fmt.Sprintf("%f:%s:%s", premium, shortTs, longTs)
+					// msg := fmt.Sprintf("%s -> %.6s (short: %.8s, long: %.8s)\n", key, value, shortPrice, longPrice)
 
 					// log.Printf("%s", msg)
 
-					if premium > 0.5 {
-						if premium < 1.0 {
-							tgmanager.SendMsg(msg)
-						}
-					}
+					// if premium > 0.5 {
+					// 	if premium < 1.0 {
+					// 		tgmanager.SendMsg(msg)
+					// 	}
+					// }
 
 					redismanager.PreHandlePremium(key, value)
-					time.Sleep(10 * time.Millisecond)
+					time.Sleep(1 * time.Millisecond)
 				}
 			}
 		}
 	}
 }
 
-func calSpotFuturePremium(pairMap map[string][]string) {
-	for {
-		for _, se := range []string{"bin", "byb"} {
-			for _, fe := range []string{"bif", "byf"} {
-				mutualPairs := commons.Intersect(pairMap[se], pairMap[fe])
+// func calSpotFuturePremium(pairMap map[string][]string) {
+// 	for {
+// 		for _, se := range []string{"bin", "byb"} {
+// 			for _, fe := range []string{"bif", "byf"} {
+// 				mutualPairs := commons.Intersect(pairMap[se], pairMap[fe])
 
-				for _, pair := range mutualPairs {
-					var pairInfo = strings.Split(pair, ":")
-					market, symbol := strings.ToLower(pairInfo[0]), strings.ToLower(pairInfo[1])
+// 				for _, pair := range mutualPairs {
+// 					var pairInfo = strings.Split(pair, ":")
+// 					market, symbol := strings.ToLower(pairInfo[0]), strings.ToLower(pairInfo[1])
 
-					spot, ok := syncMap.Load(fmt.Sprintf("%s:%s:%s", se, market, symbol))
-					if !ok {
-						log.Printf("RETRY SPOT %s:%s:%s\n", se, market, symbol)
-						time.Sleep(time.Millisecond * 100)
-						continue
-					}
+// 					spot, ok := syncMap.Load(fmt.Sprintf("%s:%s:%s", se, market, symbol))
+// 					if !ok {
+// 						log.Printf("RETRY SPOT %s:%s:%s\n", se, market, symbol)
+// 						time.Sleep(time.Millisecond * 100)
+// 						continue
+// 					}
 
-					future, ok := syncMap.Load(fmt.Sprintf("%s:%s:%s", fe, market, symbol))
-					if !ok {
-						log.Printf("RETRY FUTURE %s:%s:%s\n", fe, market, symbol)
-						time.Sleep(time.Millisecond * 100)
-						continue
-					}
+// 					future, ok := syncMap.Load(fmt.Sprintf("%s:%s:%s", fe, market, symbol))
+// 					if !ok {
+// 						log.Printf("RETRY FUTURE %s:%s:%s\n", fe, market, symbol)
+// 						time.Sleep(time.Millisecond * 100)
+// 						continue
+// 					}
 
-					// Premium 계산
-					spotPrice, futurePrice, _ := strings.Split(spot.(string), ":")[0], strings.Split(future.(string), ":")[1], strings.Split(future.(string), ":")[2]
-					spotPriceFloat, _ := strconv.ParseFloat(spotPrice, 64)
-					futurePriceFloat, _ := strconv.ParseFloat(futurePrice, 64)
-					premium := commons.RoundToDecimal((futurePriceFloat-spotPriceFloat)/spotPriceFloat, 5) * 100
+// 					// Premium 계산
+// 					spotPrice, spotTs := strings.Split(spot.(string), ":")[0], strings.Split(spot.(string), ":")[2]
+// 					futurePrice, futureTs := strings.Split(future.(string), ":")[1], strings.Split(future.(string), ":")[2]
+// 					spotPriceFloat, _ := strconv.ParseFloat(spotPrice, 64)
+// 					futurePriceFloat, _ := strconv.ParseFloat(futurePrice, 64)
+// 					premium := commons.RoundToDecimal((futurePriceFloat-spotPriceFloat)/spotPriceFloat, 5) * 100
 
-					key := fmt.Sprintf("sf:%s:%s:%s:%s", se, fe, market, symbol)
-					value := fmt.Sprintf("%f", premium)
-					msg := fmt.Sprintf("%s -> %.6s (spot: %.8s, future: %.8s)\n", key, value, spotPrice, futurePrice)
+// 					key := fmt.Sprintf("sf:%s:%s:%s:%s", se, fe, market, symbol)
+// 					value := fmt.Sprintf("%f:%s:%s", premium, spotTs, futureTs)
+// 					// msg := fmt.Sprintf("%s -> %.6s (spot: %.8s, future: %.8s)\n", key, value, spotPrice, futurePrice)
 
-					// log.Printf("%s", msg)
+// 					// log.Printf("%s", msg)
 
-					if premium > 0.5 {
-						if premium < 1.0 {
-							tgmanager.SendMsg(msg)
-						}
-					}
+// 					// if premium > 0.5 {
+// 					// 	if premium < 1.0 {
+// 					// 		tgmanager.SendMsg(msg)
+// 					// 	}
+// 					// }
 
-					redismanager.PreHandlePremium(key, value)
-					time.Sleep(10 * time.Millisecond)
-				}
-			}
-		}
-	}
-}
+// 					redismanager.PreHandlePremium(key, value)
+// 					time.Sleep(1 * time.Millisecond)
+// 				}
+// 			}
+// 		}
+// 	}
+// }
 
 func getPairMap() map[string][]string {
 	pairMap := make(map[string][]string)
-	pairMap["bin"], pairMap["bif"] = binance.GetBinExchangeInfo(), binance.GetBifExchangeInfo()
-	pairMap["byb"], pairMap["byf"] = bybit.GetBybExchangeInfo(), bybit.GetByfExchangeInfo()
+	// pairMap["bin"], pairMap["bif"] = binance.GetBinExchangeInfo(), binance.GetBifExchangeInfo()
+	// pairMap["byb"], pairMap["byf"] = bybit.GetBybExchangeInfo(), bybit.GetByfExchangeInfo()
+
+	pairMap["bif"], pairMap["byf"] = binance.GetBifExchangeInfo(), bybit.GetByfExchangeInfo()
 	return pairMap
 }
 
@@ -456,21 +502,21 @@ func Run() {
 	<-subscribeDone // 구독 완료 신호 대기
 
 	// subscribe 완료 후 receive 함수들 실행
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		go receiveBin("bin")
-	}()
+	// wg.Add(1)
+	// go func() {
+	// 	defer wg.Done()
+	// 	go receiveBin("bin")
+	// }()
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		go receiveBif("bif")
 	}()
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		go receiveByb("byb")
-	}()
+	// wg.Add(1)
+	// go func() {
+	// 	defer wg.Done()
+	// 	go receiveByb("byb")
+	// }()
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
@@ -478,7 +524,7 @@ func Run() {
 	}()
 
 	// 2개의 calculatePremium은 receive 함수 시작 후 바로 실행
-	go calSpotFuturePremium(pairMap)
+	// go calSpotFuturePremium(pairMap)
 	go calShortLongPremium(pairMap)
 
 	wg.Wait()
--- redismanager/redismanager.go
+++ redismanager/redismanager.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"strings"
 	"sync"
 	"time"
 
@@ -45,6 +46,8 @@ func client() *redis.Client {
 
 		_, err := rdb.Ping(ctx).Result()
 		tgmanager.HandleErr("redis", err)
+
+		// client().FlushAll(ctx) // init flushall
 	})
 	return rdb
 }
@@ -63,120 +66,16 @@ func newPremium(key string, value string) *premium {
 }
 
 func (p *premium) setPremium() {
-	// TODO. if init set
+	tradeType, exchangeA, exchangeB, market, symbol := strings.Split(p.key, ":")[0], strings.Split(p.key, ":")[1], strings.Split(p.key, ":")[2], strings.Split(p.key, ":")[3], strings.Split(p.key, ":")[4]
 
-	err := client().Set(ctx, p.key, p.value, 0).Err()
+	// add pair info on SET
+	err := client().SAdd(ctx, fmt.Sprintf("%s_target", tradeType), fmt.Sprintf("%s:%s:%s:%s", exchangeA, exchangeB, market, symbol), nil).Err()
 	tgmanager.HandleErr(p.key, err)
+
+	// set each pair's value on STRING
+	err2 := client().Set(ctx, p.key, p.value, 0).Err()
+	tgmanager.HandleErr(p.key, err2)
+
 	log.Printf("REDIS SET %s -> %s\n", p.key, p.value)
 
 }
-
-// func PreHandleOrderbook(api, exchange, market, symbol string, askSlice, bidSlice []interface{}, ts string) {
-// 	ob := newOrderbook(exchange, market, symbol, ts)
-
-// 	// targetVolume := commons.GetTargetVolumeMap(exchange)[market+":"+symbol]
-// 	// askPrice, bidPrice := commons.GetObTargetPrice(targetVolume, askSlice), commons.GetObTargetPrice(targetVolume, bidSlice)
-// 	// ob.askPrice, ob.bidPrice = askPrice, bidPrice
-
-// 	ob.setOrderbook(api)
-// }
-
-// func newOrderbook(exchange, market, symbol, ts string) *orderbook {
-// 	ob := &orderbook{
-// 		exchange: exchange,
-// 		market:   market,
-// 		symbol:   symbol,
-// 		askPrice: "",
-// 		bidPrice: "",
-// 		ts:       ts,
-// 		bsTs:     "",
-// 	}
-// 	return ob
-// }
-
-// func (ob *orderbook) setOrderbook(api string) {
-// 	now := time.Now().In(location).Format(StampMicro)
-// 	obTs, errParseInt := strconv.ParseInt(ob.ts, 10, 64)
-// 	tgmanager.HandleErr(ob.exchange, errParseInt)
-
-// 	// if init
-// 	prevObTs, ok := syncMap.Load(fmt.Sprintf("%s:%s", ob.market, ob.symbol))
-// 	if !ok {
-// 		fmt.Printf("REDIS init set of %s:%s\n", ob.market, ob.symbol)
-// 		syncMap.Store(fmt.Sprintf("%s:%s", ob.market, ob.symbol), int(obTs))
-// 		// prevObTs = int(obTs)
-// 	} else {
-// 		// get&set ts of bookstore
-// 		bsTsStr := commons.FormatTs(fmt.Sprintf("%d", time.Now().UnixNano()/100000))
-// 		bsTs, errParseInt := strconv.ParseInt(bsTsStr, 10, 64)
-// 		tgmanager.HandleErr(ob.exchange, errParseInt)
-
-// 		ob.bsTs = bsTsStr
-
-// 		// set redis
-// 		key := fmt.Sprintf("ob:%s:%s:%s", ob.exchange, ob.market, ob.symbol)
-// 		var value string
-// 		// value := fmt.Sprintf("%s|%s|%s|%s", ob.ts, ob.askPrice, ob.bidPrice, ob.bsTs)
-// 		// value := fmt.Sprintf("%s|%s|%s", ob.ts, ob.askPrice, ob.bidPrice)
-
-// 		obTsGap := int(obTs) - prevObTs.(int)
-// 		bsTsGap := int(bsTs) - int(obTs)
-
-// 		// set only when gap of bs / ob within range
-// 		// if bsTsGap > 3*1000 {
-// 		// 	errMsgCnt += 1
-// 		// 	errMsg += fmt.Sprintf("%s:%dms\nbs: %d / ob: %d\n\n", key, bsTsGap, bsTs, obTs)
-// 		// 	if errMsgCnt == 10 {
-// 		// 		tgmanager.SendMsg(fmt.Sprintf("%s%s", "## BS-OB LATENCY ##\n\n", errMsg))
-// 		// 		errMsgCnt = 0
-// 		// 		errMsg = ""
-// 		// 	}
-
-// 		// 	// fmt.Printf("\npvTs: %d\n", prevObTs)
-// 		// 	// fmt.Printf("obTs: %d\n", obTs)
-// 		// 	// fmt.Printf("bsTs: %d\n", bsTs)
-
-// 		// } else {
-
-// 		if obTsGap > 0 { // 거래소별 서버 ts 기준, 최신 호가 정보를 저장하고
-// 			value = fmt.Sprintf("%s|%s|%s|%s", ob.ts, ob.askPrice, ob.bidPrice, ob.bsTs)
-// 			err := client().Set(ctx, key, value, 0).Err()
-// 			tgmanager.HandleErr(ob.exchange, err)
-
-// 			syncMap.Store(fmt.Sprintf("%s:%s", ob.market, ob.symbol), int(obTs))
-// 			fmt.Printf("%s Set %s %s %4dms %4dms %4s %4s %4s %4s\n", now, api, key, obTsGap, bsTsGap, ob.ts, ob.bsTs, ob.askPrice, ob.bidPrice)
-
-// 		} else if obTsGap == 0 && bsTsGap > 0 { // 장기간 호가 변동 없을 시, bookstore의 bs를 저장한다 (syncMap은 저장하지 않음)
-// 			value = fmt.Sprintf("%s|%s|%s|%s", ob.bsTs, ob.askPrice, ob.bidPrice, ob.bsTs)
-// 			err := client().Set(ctx, key, value, 0).Err()
-// 			tgmanager.HandleErr(ob.exchange, err)
-
-// 			fmt.Printf("%s Rnw %s %s %4dms %4dms %4s %4s %4s %4s\n", now, api, key, obTsGap, bsTsGap, ob.ts, ob.bsTs, ob.askPrice, ob.bidPrice)
-
-// 		} else {
-// 			fmt.Printf("%s >>> %s %s %4dms %4dms (obTsGap / bsTsGap)\n", now, api, key, obTsGap, bsTsGap) // 이전의 goroutine이 도달하는 경우 obTsGap 음수값 리턴 가능
-// 		}
-// 	}
-
-// 	// } else if realTsGap > 800 { // refresh - considering low traded coin, set if allowed time has past
-// 	// 	value2 := fmt.Sprintf("%s|%s|%s", bsTsStr, ob.askPrice, ob.bidPrice)
-// 	// 	err2 := client().Set(ctx, key, value2, 0).Err()
-// 	// 	tgmanager.HandleErr(ob.exchange, err2)
-
-// 	// 	syncMap.Store(fmt.Sprintf("%s:%s", ob.market, ob.symbol), int(realTs))
-// 	// 	fmt.Printf("%s Ref %s %s %4dms %4s %4s %4s %4s\n", now, api, key, realTsGap, bsTsStr, ob.askPrice, ob.bidPrice, ob.bsTs)
-
-// 	// 	// logType = "R" // refresh
-
-// 	// logTs = ob.ts
-// 	// if logType == "R" {
-// 	// 	logTs = realTsStr
-// 	// }
-
-// 	// if logType == "R" || logType == "N" {
-// 	// 	go alog.TraceLog(
-// 	// 		ob.exchange,
-// 	// 		fmt.Sprintf("%s|%s|%s|%s|%s|%s", logType, logTs, ob.market, ob.symbol, ob.askPrice, ob.bidPrice),
-// 	// 	)
-// 	// }
-// }
